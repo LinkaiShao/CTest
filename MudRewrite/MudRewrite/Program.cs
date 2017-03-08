6c7467bf03228d71f6db774d8b565a1c6e23f0fd
LinkaiShao <linkai.k.shao@gmail.com>
2017-03-07 18:33:20 -0800

    dwa

---
+++ MudRewrite/MudRewrite/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+using System.Collections.Generic;
+namespace MudRewrite
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string[] read = File.ReadAllLines ("Prelogue1");
+			string name = read [0];
+			string description = read [1];
+			string[]moves=new string[6];
+			int ResultPlace=0;
+			for (int i = 3; i < read.Length; i++) {
+				if (read [i] != "Results:") {
+					moves [i - 3] = read [i];
+				} else {
+					ResultPlace = i;
+					break;
+				}
+			}
+			int CharacterPlace=0;
+			string[] results = new string[6];
+			for (int i = ResultPlace + 1; i < read.Length; i++) {
+				if (read [i] != "Characters:") {
+					results [i - ResultPlace - 1] = read [i];
+				} else {
+					CharacterPlace = i;
+					break;
+				}
+			}
+			string[] Characters = new string[6];
+			int GlancePlace=0;
+			for (int i = CharacterPlace + 1; i < read.Length; i++) {
+				if (read [i] != "Glance:") {
+					Characters [i - CharacterPlace - 1] = read [i];
+				} else {
+					GlancePlace = i;
+					break;
+				}
+			}
+			string Glance;
+			if (read [GlancePlace+1] != "Glance:") {
+				Glance = read [GlancePlace + 1];
+			}
+			Room()
+
+		}
+	}
+}
---
+++ MudRewrite/MudRewrite/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MudRewrite
+{
+	public class Room
+	{
+		
+		public string Name;
+		public string Description;
+		public string[]Moves=new string[6];
+		public string[]Results=new string[6];
+		public string[]Characters=new string[6];
+		public string Glance;
+		public Room(String name,string description,string[]moves,string[]results,string[]characters,string glance){
+			name = this.Name;
+			description = this.Description;
+			moves = this.Moves;
+			results = this.Results;
+			characters = this.Characters;
+			glance = this.Glance;
+		}
+	
+
+	}
+}
+
